562227d5a482ba22f5753c4acf69d54c055d79ca
yooningu <yig0717@naver.com>
2025-05-15 19:03:02 +0900

    로그인 페이지 추가

--- ex/src/index.js
+++ ex/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Registration from './ex/Registration.jsx';
 import reportWebVitals from './reportWebVitals';
+import Loginpage from './ex/Loginpage.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Registration />
+    <Loginpage />
   </React.StrictMode>
 );
 
