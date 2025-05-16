30bc521c9bd24141943ce579ee129a77d8580f11
yooningu <yig0717@naver.com>
2025-05-16 14:47:43 +0900

    .

--- ex/src/index.js
+++ ex/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Registration from './ex/Registration.jsx';
+import Particular from './ex/Particular.jsx'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Registration />
+    <Particular />
   </React.StrictMode>
 );
 
